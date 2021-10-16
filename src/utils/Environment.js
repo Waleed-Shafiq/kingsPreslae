800ad69d496712a3809c196c60a1e3e87fdc4cc6
Itay Edry <itayedry@Itays-MacBook-Air.local>
2021-10-16 12:06:25 +0300

    check

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,5 +1,5 @@
 let Environment = {
-    kingsAddress: "0x918d1f38AC2a642FA75E6a5efC9317018292E6e1",
+    kingsAddress: "0x832De7fe8147e400476834193857Ab0C8B520779",
   };
   
   export default Environment;
